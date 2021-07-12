a82d6ad6bc616801250d2782fae6717ce976bae7
Igor Lucas <igorescritor@hotmail.com>
2021-07-12 17:23:50 -0300

    Mapeando Pedido e usando ManyToOne e OneToMany

--- Spring_Boot_Expert/src/main/java/com/igorlucas/entity/Cliente.java
+++ Spring_Boot_Expert/src/main/java/com/igorlucas/entity/Cliente.java
@@ -1,10 +1,13 @@
 package com.igorlucas.entity;
 
+import java.util.Set;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -18,6 +21,9 @@ public class Cliente {
 	
 	@Column(name = "nome", length = 100)
 	private String nome;
+	
+	@OneToMany(mappedBy = "cliente")
+	private Set<Pedido> pedidos;
 	
 	public Cliente() {}
 	
@@ -41,6 +47,14 @@ public class Cliente {
 	
 	public void setNome(String nome) {
 		this.nome = nome;
+	}
+
+	public Set<Pedido> getPedidos() {
+		return pedidos;
+	}
+
+	public void setPedidos(Set<Pedido> pedidos) {
+		this.pedidos = pedidos;
 	}
 
 	@Override
