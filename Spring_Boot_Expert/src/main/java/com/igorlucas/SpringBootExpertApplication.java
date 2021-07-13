980f1b23aab1c069145cfb4cf77dfbbf5b74c811
Igor Lucas <igorescritor@hotmail.com>
2021-07-13 00:21:40 -0300

    Criando e testando um método GET

--- Spring_Boot_Expert/src/main/java/com/igorlucas/SpringBootExpertApplication.java
+++ Spring_Boot_Expert/src/main/java/com/igorlucas/SpringBootExpertApplication.java
@@ -1,10 +1,24 @@
 package com.igorlucas;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.igorlucas.entity.Cliente;
+import com.igorlucas.repository.Clientes;
 
 @SpringBootApplication
 public class SpringBootExpertApplication {
+	
+	@Bean
+	public CommandLineRunner commandLineRunner(@Autowired Clientes clientes) {
+		return args -> {
+			Cliente c = new Cliente(null, "Igor");
+			clientes.save(c);
+		};
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootExpertApplication.class, args);
