1ddfcfd97f5be6a0df07645af3da78667847bd75
Igor Lucas <igorescritor@hotmail.com>
2021-07-13 21:40:34 -0300

    Criando post, put e delete

--- Spring_Boot_Expert/src/main/java/com/igorlucas/controllers/ClienteController.java
+++ Spring_Boot_Expert/src/main/java/com/igorlucas/controllers/ClienteController.java
@@ -5,8 +5,12 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -30,6 +34,36 @@ public class ClienteController {
 		return ResponseEntity.notFound().build();
 	}
 	
+	@PostMapping
+	@ResponseBody
+	public ResponseEntity<Cliente> save (@RequestBody Cliente cliente) {
+		Cliente clienteSalvo = clientes.save(cliente);
+		return ResponseEntity.ok(clienteSalvo);
+	}
 	
+	@DeleteMapping(value = "/{id}")
+	@ResponseBody
+	public ResponseEntity delete(@PathVariable Integer id) {
+		Optional<Cliente> cliente = clientes.findById(id);
+		if (cliente.isPresent()) {
+			clientes.delete(cliente.get());
+			return ResponseEntity.noContent().build();
+		} else {
+			return ResponseEntity.notFound().build();
+		}
+	}
+	
+	
+	@PutMapping(value = "/{id}")
+	@ResponseBody
+	public ResponseEntity<Cliente> update(@PathVariable Integer id, @RequestBody Cliente cliente ) {
+		return clientes
+				.findById(id)
+				.map(c -> {
+					cliente.setId(c.getId());
+					clientes.save(cliente);
+					return ResponseEntity.ok(cliente);
+				}).orElseGet(() -> ResponseEntity.notFound().build());
+	}
 
 }
